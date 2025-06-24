8d256564f06947c39258c730216605745b2b01e9
Subham <subhamkumarshee@gmail.com>
2025-06-24 15:59:54 +0530

    added my old project game hub to this

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ import { Project } from '../types';
 
 export const projects: Project[] = [
   {
-    id: 'project-6',
+    id: 'project-8',
     title: 'RNA Hub',
     description: 'This project explores the integration of RNA 3D structural information with gene expression data to enhance cancer detection methodologies',
     image: 'https://res.cloudinary.com/dpa0sb1tm/image/upload/v1745566595/background_czwzgo.jpg',
@@ -12,7 +12,7 @@ export const projects: Project[] = [
     featured: true,
   },
   {
-    id: 'project-5',
+    id: 'project-7',
     title: 'Safema',
     description: 'Safema is a revolutionary women\'s safety app that combines smart wearable devices with community protection features for immediate assistance in emergencies.',
     image: 'https://res.cloudinary.com/dpa0sb1tm/image/upload/c_crop,w_650,h_500,g_auto/v1747552956/32682d32-f5a0-4530-a1c8-e97b51423757_coyfvr.jpg',
@@ -22,7 +22,7 @@ export const projects: Project[] = [
     featured: true,
   },
   {
-    id: 'project-4',
+    id: 'project-6',
     title: 'GRIET Study Sphere',
     description: 'GRIET Study Sphere is a centralized platform offering curated academic resources, notes, and tools to support GRIET students in their learning journey.',
     image: 'https://res.cloudinary.com/dpa0sb1tm/image/upload/c_crop,w_1500,h_1000,g_auto/v1750088930/Screenshot_2025-06-16_211824_awwurj.png',
@@ -32,7 +32,7 @@ export const projects: Project[] = [
     featured: true,
   },
   {
-    id: 'project-3',
+    id: 'project-5',
     title: 'Task Talk',
     description: 'Task Talk is a smart task management app that lets you set voice reminders for each task, making your to-do list more personal and effective.',
     image: 'https://images.pexels.com/photos/3183165/pexels-photo-3183165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -42,7 +42,7 @@ export const projects: Project[] = [
     featured: false,
   },
   {
-    id: 'project-2',
+    id: 'project-4',
     title: 'FSW club website',
     description: 'Official website for the Free Software Wing club at GRIET, built to showcase events, projects, and promote open-source initiatives.',
     image: 'projects/fsw-club.png',
@@ -52,7 +52,7 @@ export const projects: Project[] = [
     featured: false,
   },
   {
-    id: 'project-1',
+    id: 'project-3',
     title: 'Netflix Clone',
     description: 'A simple Netflix clone built for fun, featuring user authentication, movie browsing, and a responsive UI using Firebase and React.',
     image: 'projects/netflix-clone.webp',
@@ -62,7 +62,17 @@ export const projects: Project[] = [
     featured: false,
   },
   {
-    id: 'project-7',
+    id: 'project-2',
+    title: 'Game Hub',
+    description: 'Subham Game Web is a fun and interactive browser-based collection of classic and casual games built using React. It provides a lightweight, responsive interface where users can quickly switch between multiple mini-games.',
+    image: 'projects/gamehub.png',
+    tags: ['React', 'Tailwind CSS'],
+    link: 'https://subham-gamehub.vercel.app/',
+    github: 'https://github.com/Subham8705/my-game-hub',
+    featured: false,
+  },
+  {
+    id: 'project-1',
     title: 'Parallax Project-1',
     description: 'Created a dynamic parallax landing page with layered animations and custom Photoshop-edited assets, featuring glassmorphic UI and responsive design.',
     image: 'projects/parallax1.gif',
